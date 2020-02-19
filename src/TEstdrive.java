d95e90b095a1041b3c24042f9b3e28645e54362c
yourUsername <yourEmail@domain.com>
2020-02-19 17:16:27 -0500

    gg

---
+++ src/TEstdrive.java
@@ -0,0 +1,36 @@
+public class TEstdrive {
+
+public static void main(String[]args) {
+
+   int[] numbers = {10, 20,30,40,50};
+
+// let the printer print out all the numbe values
+for(int i = 0; i<4; i++){
+    System.out.println( numbers [i]);
+//break;
+    continue;
+
+}
+System.out.println( "----------------------" );
+int i = 0;
+
+    while (i < 3){
+        System.out.println( "Hi" );
+     i++;
+
+       // break;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+}
